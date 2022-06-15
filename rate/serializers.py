4697fded515bcd5b42346ab9cf1c01ee871ee863
Hanan-Hussein <hanhuss035@gmail.com>
2022-06-15 10:49:54 +0300

    ft:profile-created profile serializer

--- rate/serializers.py
+++ rate/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
-from .models import Project
+from .models import Project, Profile
 
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model=Project
+        fields = '__all__'
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Profile
         fields = '__all__'
